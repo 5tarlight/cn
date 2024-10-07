a9146a3348400c938ee301b9706a0cbf0d7ceba4
YEAHx4 <developerstarlight@gmail.com>
2024-10-08 01:32:09 +0900

    Support cjs

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,5 @@
-export default function cn(...classNames: (string | undefined)[]): string {
+function cn(...classNames: (string | undefined)[]): string {
   return classNames.filter(Boolean).join(" ");
 }
+
+export default cn;
